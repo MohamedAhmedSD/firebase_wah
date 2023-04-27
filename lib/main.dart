c3433fe8a108a0f5fe6ad0fea36306d426e84514
MohamedAhmedSD <btoox8python@gmail.com>
2023-04-27 15:39:29 +0200

    [app note - sign in && sign up - start point]

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:wah_firebase/firebase_options.dart';
 
-import 'auth/github2.dart';
+import 'noteapp/auth/login.dart';
+import 'noteapp/auth/signup.dart';
+import 'noteapp/crud/addnotes.dart';
+import 'noteapp/home/homepage.dart';
+import 'noteapp/testtwo.dart';
 
 //? ======== notes ===================
 //! we can get SHA also by go => cd android  ,then write =>
@@ -21,6 +27,14 @@ import 'auth/github2.dart';
 //* why we use firebase core ?
 //? it connect our app to firebase services
 
+//? =========================
+bool? islogin;
+
+Future backgroudMessage(RemoteMessage message) async {
+  print("=================== BackGroud Message ========================");
+  print("${message.notification!.body}");
+}
+
 //? ======== connect to FB ===================
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,6 +42,16 @@ Future<void> main() async {
       //* you can change options according to your device
       // options: DefaultFirebaseOptions.currentPlatform,
       options: DefaultFirebaseOptions.android);
+
+  //? =========
+  FirebaseMessaging.onBackgroundMessage(backgroudMessage);
+
+  var user = FirebaseAuth.instance.currentUser;
+  if (user == null) {
+    islogin = false;
+  } else {
+    islogin = true;
+  }
   runApp(const MyApp());
 }
 
@@ -38,13 +62,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FireBase',
+      debugShowCheckedModeBanner: false,
+      // home: islogin == false ? Login() : HomePage(),
+      home: Login(),
+      // home: Test(),
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(
-        title: '',
-      ),
+          // fontFamily: "NotoSerif",
+          primaryColor: Colors.blue,
+          textTheme: TextTheme(
+            titleLarge: TextStyle(fontSize: 20, color: Colors.white),
+            headlineSmall: TextStyle(fontSize: 30, color: Colors.blue),
+            bodyMedium: TextStyle(fontSize: 20, color: Colors.black),
+          )),
+      routes: {
+        "login": (context) => Login(),
+        "signup": (context) => SignUp(),
+        "homepage": (context) => HomePage(),
+        "addnotes": (context) => AddNotes(),
+        "testtwo": (context) => TestTwo()
+      },
     );
   }
 }
